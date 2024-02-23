0f29d090c57a7f784da7962474e8cabeacde7a3f
ColaniYT <125219455+ColaniYT@users.noreply.github.com>
2024-02-23 17:50:59 +0100

    Delete nvim directory
    removed main

--- nvim/lua/user/colorscheme.lua
+++
@@ -1,8 +0,0 @@
--- local colorscheme = "catppuccin-frappe"
-local colorscheme = "dracula"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " does not exist.")
-  return
-end
