2eba9a8b5a4b6ce501780c54b63090a067e148d2
Christopher <125219455+ColaniYT@users.noreply.github.com>
2024-03-06 22:44:24 +0100

    another possible fix

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -9,7 +9,7 @@ if not snip_status_ok then
 end
 
 luasnip.config.set_config {
-  history = false,
+  history = true,
   region_check_events = "InsertEnter",
   delete_check_events = "TextChanged,InsertLeave",
 }
@@ -79,8 +79,8 @@ cmp.setup {
         cmp.select_next_item()
       elseif luasnip.expandable() then
         luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+      --[[ elseif luasnip.expand_or_jumpable() then ]]
+      --[[   luasnip.expand_or_jump() ]]
       elseif check_backspace() then
         fallback()
       else
@@ -93,8 +93,8 @@ cmp.setup {
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+      --[[ elseif luasnip.jumpable(-1) then ]]
+      --[[   luasnip.jump(-1) ]]
       else
         fallback()
       end
